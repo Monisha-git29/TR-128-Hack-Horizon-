0436ce76a01918523a577ebf4835034e281b99a5
Monisha <monishavnreddy07@gmail.com>
2026-04-17 05:58:04 +0530

    final clean api fix

--- api/index.py
+++ api/index.py
@@ -1,15 +1,11 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI
 
 app = FastAPI()
 
 @app.get("/")
-async def root(request: Request):
-    path = request.query_params.get("path")
-
-    if path == "hello":
-        return {"msg": "Hello from backend"}
-
+async def root():
     return {"message": "Backend working 🚀"}
 
-# required for vercel
-handler = app
+@app.get("/hello")
+async def hello():
+    return {"msg": "Hello from backend"}
