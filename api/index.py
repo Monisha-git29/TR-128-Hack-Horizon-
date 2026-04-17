1db031cc5b852a84c344946cdd33893efba82a9b
Monisha <monishavnreddy07@gmail.com>
2026-04-17 05:45:21 +0530

    fix api routing for vercel

--- api/index.py
+++ api/index.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/")
+# (optional but good practice)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.get("/api")
 def root():
     return {"message": "Backend working 🚀"}
 
-@app.get("/hello")
+@app.get("/api/hello")
 def hello():
     return {"msg": "Hello from backend"}
