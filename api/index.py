7bb6fa9c1640c280664eface9b4c085e3a4682ed
Monisha <monishavnreddy07@gmail.com>
2026-04-17 05:52:38 +0530

    single route backend fix

--- api/index.py
+++ api/index.py
@@ -1,14 +1,15 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
+async def root(request: Request):
+    path = request.query_params.get("path")
+
+    if path == "hello":
+        return {"msg": "Hello from backend"}
+
     return {"message": "Backend working 🚀"}
 
-@app.get("/hello")
-async def hello():
-    return {"msg": "Hello from backend"}
-
-# 👇 IMPORTANT for Vercel
+# required for vercel
 handler = app
